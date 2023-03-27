43d20ebfe0f2fed8ebb5bf1f0af846d8344aeecf
Neonx <fabiangomes2015@gmail.com>
2023-03-27 18:36:22 -0500

    Adding a button test

--- app/src/main/java/com/example/datos/MainActivity.java
+++ app/src/main/java/com/example/datos/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.datos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText Operacional;
+
+    TextView Opcion;
+
+    Button Test;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Operacional = (EditText)findViewById(R.id.InputOperacion);
+
+        Opcion = (TextView) findViewById(R.id.Opcional);
+
+        Test = (Button) findViewById(R.id.test);
+
+        Operacional.setOnClickListener(this);
     }
 
     //Metodo Boton El boton siguiente
@@ -20,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
     public void Siguiente(View view){
         Intent siguiente = new Intent(this,Resultado.class);
         startActivity(siguiente);
+    }
+
+    @Override
+    public void onClick(View view) {
+        float Operacion;
+
+
+        Operacion = Float.parseFloat(Operacional.getText().toString());
+
+        switch (view.getId()){
+
+            case    R.id.test:
+                Opcion.setText("El numero Seleccionado es: "+ Operacion);
+        }
     }
 }
--- app/src/main/java/com/example/datos/Resultado.java
+++ app/src/main/java/com/example/datos/Resultado.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Resultado extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+
+
+
+
     }
     //metodo para regresar
     public void Anterior(View view){
         Intent anterior = new Intent(this, MainActivity.class);
         startActivity(anterior);
+
+
+
     }
 }
